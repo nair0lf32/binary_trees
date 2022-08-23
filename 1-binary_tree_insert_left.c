06609718308cb52e445fd3dadf6b52a81c4d59a8
nair0lf32 <florianedem@gmail.com>
2022-08-23 22:02:32 +0100

    fix task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,15 +8,15 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *node;
+if (parent == NULL)
+{
+free(parent);
+return (NULL);
+}
 node = binary_tree_node(parent, value);
 if (node == NULL)
 {
 free(node);
-return (NULL);
-}
-if (parent == NULL)
-{
-free(parent);
 return (NULL);
 }
 if (parent->left != NULL)
