ea022f9c771ba835c1eece3b783f5b28a73c9514
nair0lf32 <florianedem@gmail.com>
2022-08-23 21:53:49 +0100

    do task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+*binary_tree_insert_left - inserts a node as the left-child of another node
+*@parent: pointer to the parent node to insert on
+*@value: value for the new node
+*Return: return a pointer to the created node or NULL if failure
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *node;
+node = malloc(sizeof(binary_tree_t));
+if (node == NULL)
+{
+free(node);
+return (NULL);
+}
+if (parent == NULL)
+{
+free(parent);
+return (NULL);
+}
+if (parent->left != NULL)
+{
+node->left = parent->left;
+node->left->parent = node;
+}
+parent->left = node;
+return (node);
+}
