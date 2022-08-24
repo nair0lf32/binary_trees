5d8e2f8b0975f9a0c01d7d7a3caff0284def5764
nair0lf32 <florianedem@gmail.com>
2022-08-24 20:28:52 +0100

    fix task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,8 +8,8 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 if (tree != NULL)
 {
-free(tree->left);
-free(tree->right);
+binary_tree_delete(tree->left);
+binary_tree_delete(tree->right);
 free(tree);   
 }
 }
