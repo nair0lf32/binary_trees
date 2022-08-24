94854f0062d25989d8b065a728617e8ed75ece34
nair0lf32 <florianedem@gmail.com>
2022-08-24 20:44:37 +0100

    fix task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,10 +8,10 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 if (node == NULL || node->left != NULL || node->right != NULL)
 {
-return(0);
+return (0);
 }
 else
 {
-return(1);
+return (1);
 }
 }
