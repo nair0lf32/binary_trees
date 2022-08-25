122fb0ddf31a65cb16257ee6d6b35b697c08fa1b
nair0lf32 <florianedem@gmail.com>
2022-08-25 18:56:55 +0100

    fixing task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,26 +6,18 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t left_nodes = 0, right_nodes = 0;
 if (tree == NULL)
 {
 return (0);
 }
-size_t left_nodes, right_nodes;
 if (tree->left != NULL)
 {
 left_nodes = binary_tree_height(tree->left) + 1;
 }
-else
-{
-left_nodes = 0;
-}
 if (tree->right != NULL)
 {
 right_nodes = binary_tree_height(tree->right) + 1;
-}
-else
-{
-right_nodes = 0;
 }
 if (left_nodes >= right_nodes)
 {
